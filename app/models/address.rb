fda6a0256f7582fe05139c1b4a114d6b13e72bfa
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-27 12:43:25 -0600

    Add coverage to gitignore. Thanks

---
+++ app/models/address.rb
@@ -0,0 +1,9 @@
+class Address < ApplicationRecord
+  validates_presence_of :street
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
+  validates_presence_of :nickname
+
+  belongs_to :user
+end
---
+++ db/migrate/20190727160440_remove_columns_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveColumnsFromUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :city
+    remove_column :users, :state
+    remove_column :users, :zip
+  end
+end
