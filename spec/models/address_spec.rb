6b1cb5a0a3b40c63790b196af24175c9e4f95ac6
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-29 19:55:43 -0600

    Add test for shipped to to achieve 100 percent testing on models

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -25,4 +25,22 @@ RSpec.describe Address do
       expect(user.addresses).to eq([address, addy_2])
     end
   end
+
+  describe 'instance method' do
+    it ".shipped_to?" do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      user = User.create!(name: "Kirren", email: "yooo@email.com", password: "password")
+      address = user.addresses.create!(street: "Street", city: "city", state: "state", zip: 98765)
+      addy_2 = user.addresses.create!(street: "Fivestar", city: "Nashyville", state: "Tennessee", zip: 12345, nickname: 1)
+      @order_1 = user.orders.create!(address_id: address.id, status: "pending")
+      @order_2 = user.orders.create!(address_id: addy_2.id, status: "shipped")
+
+      expect(addy_2.shipped_to?).to be_truthy
+      expect(address.shipped_to?).to be_falsy
+    end
+  end
 end
